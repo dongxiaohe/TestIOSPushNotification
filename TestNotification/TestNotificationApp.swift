96a3cdf6257c4da21ff8c41c1072294e10333af0
Xiaohe Dong <dannyrivclo@gmail.com>
2021-04-13 18:46:41 -0700

    Initial Commit

---
+++ TestNotification/TestNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNotificationApp.swift
+//  TestNotification
+//
+//  Created by Xiaohe Dong on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct TestNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
